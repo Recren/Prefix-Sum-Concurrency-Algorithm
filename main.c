9fac42213de071fc5fb95208f4e3dee960d5fedb
Steven <124327017+Recren@users.noreply.github.com>
2024-06-17 01:03:54 -0500

    Implemented the dynamic array to store the values of input file

--- main.c
+++ main.c
@@ -6,11 +6,18 @@
 #include <errno.h>
 
 const int MEMORY_SIZE = 4096;
+int inputArraySize = 0;
 
-int checkInputNumberMatches(FILE *file, int numElements)
+void errormsg( char *msg ) {
+   perror( msg );
+   exit( 1 );
+}
+
+int checkInputNumberMatches(FILE *file, int numElements, int **arr)
 {
     int numsInFile = 0;
     int num;
+    *arr = NULL;
     // Read through file and tally the amount of numbers in it
     while (fscanf(file, "%d", &num) != EOF)
     {
@@ -19,14 +26,41 @@ int checkInputNumberMatches(FILE *file, int numElements)
         // If they differ, exit program
         if (numsInFile > numElements)
         {
+            free(*arr);
             printf("Number of elements in file is greater than number of elements user specified\n");
             return 0;
+        }
+        //Read the number into the array
+        else{    
+            inputArraySize++;
+
+            //Initial array allocation to store one elemenet
+            if(numsInFile == 1){
+                *arr = (int *)malloc(1 * sizeof(int));
+                //Check if memory allocation failed
+                if(*arr == NULL) {
+                    printf("Memory allocation failed.\n");
+                    return 0;
+                }
+            }
+            //As we read more numbers from the file, grow our array by one each time
+            else{
+                *arr = (int *)realloc(*arr, numsInFile * sizeof(int));
+                //Check if memory allocation failed
+                if(*arr == NULL) {
+                    printf("Memory allocation failed.\n");
+                    return 0;
+                }
+            }
+            //Store the number into the array
+            (*arr)[numsInFile - 1] = num;
         }
     }
     
     // If they differ, exit program
     if (numsInFile != numElements)
     {
+        free(*arr);
         printf("Number of elements in file is less than number of elements user specified\n");
         return 0;
     }
@@ -65,14 +99,14 @@ void spawnProcesses(int numCores, pid_t pidOfRoot)
 int main()
 {
 
-    int segment_id;
+    // int segment_id;
 
-    // create a memory segment to be shared
-    segment_id = shmget( IPC_PRIVATE, MEMORY_SIZE, S_IRUSR | S_IWUSR );
+    // // create a memory segment to be shared
+    // segment_id = shmget( IPC_PRIVATE, MEMORY_SIZE, S_IRUSR | S_IWUSR );
 
-    if ( segment_id < 0 ) errormsg( "ERROR in creating a shared memory segment\n" );
+    // if ( segment_id < 0 ) errormsg( "ERROR in creating a shared memory segment\n" );
 
-    fprintf( stdout, "Segment id = %d\n", segment_id );
+    // fprintf( stdout, "Segment id = %d\n", segment_id );
 
     // Must make sure value is greater than zero
     int numElements;
@@ -126,12 +160,26 @@ int main()
         }
     }
 
-    // Check if the input number matches the number of elements in the file
-    int numElementsInFile = checkInputNumberMatches(file, numElements);
+    // Check if the input number matches the number of elements in the file and write the elements into the array
+    //Our array will have to be dynamic and grow based on size of input file
+    int *inputArray;
+    int numElementsInFile = checkInputNumberMatches(file, numElements, &inputArray);
+
     //Exit program if we have invalid number of elements
     if (numElementsInFile == 0){return 1;}
     //If we have more cores then elements in the file, trim down the number of cores
     else if(numElementsInFile < numCores){numCores = numElementsInFile;}
+
+    // int j = 0;
+    // for(j = 0; j < inputArraySize; j++){
+    //     printf("%d", inputArray[j]);
+    // }
+    //Create a barrier for m amount of cores
+    int barrier[numCores];
+    int i;
+    for(i = 0; i < numCores; i++){
+        barrier[i] == 0;
+    }
     // // If we reach here, we are ready to begin
     pid_t pidOfRoot = getpid();
     spawnProcesses(numCores, pidOfRoot);
